b0aa43315b27268cf467412f673ef8921e1dcfe4
Rishabh Ballal <rishabhballal@gmail.com>
2025-12-02 12:46:08 +0530

    fix: geometric asian option payoffs

--- payoffs.py
+++ payoffs.py
@@ -1,5 +1,6 @@
-# some standard option payoffs
+import numpy as np
 
+# European, Bermudan, American option payoffs
 forward_contract = lambda K: (lambda S: S-K)
 
 vanilla_call = lambda K: (lambda S: S-K if S>K else 0)
@@ -13,5 +14,13 @@ power_put = lambda K, l: (lambda S: (K-S)^l if S<K else 0)
 
 straddle = lambda K: (lambda S: S-K if S>K else K-S)
 
-asian_call = lambda K: (lambda S: sum(S)/len(S) - K if sum(S)/len(S) > K else 0)
-asian_put = lambda K: (lambda S: K - sum(S)/len(S) if sum(S)/len(S) < K else 0)
+# Asian option payoffs
+arithmetic_asian_call = lambda K: (lambda S: sum(S)/len(S) - K \
+    if sum(S)/len(S) > K else 0)
+arithmetic_asian_put = lambda K: (lambda S: K - sum(S)/len(S) \
+    if sum(S)/len(S) < K else 0)
+
+geometric_asian_call = lambda K: (lambda S: np.prod(S)**(1/len(S)) - K \
+    if np.prod(S)**(1/len(S)) > K else 0)
+geometric_asian_put = lambda K: (lambda S: K - np.prod(S)**(1/len(S)) \
+    if np.prod(S)**(1/len(S)) < K else 0)
