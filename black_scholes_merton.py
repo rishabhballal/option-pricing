5b6c093da52d4dc086eb77803e233c780762c355
Rishabh Ballal <rishabhballal@gmail.com>
2025-12-03 17:50:43 +0530

    style: replace lambda functions

--- black_scholes_merton.py
+++ black_scholes_merton.py
@@ -2,9 +2,10 @@ import math
 import numpy as np
 from scipy.stats import norm
 
-_gaussian = lambda x: math.exp(-0.5 * (x**2))/math.sqrt(2 * math.pi)
+def _gaussian(x):
+    return math.exp(-0.5 * (x**2))/math.sqrt(2 * math.pi)
 
-def _normal_limits(func):
+def _normal_cdf_limits(func):
     def wrapper(self):
         d_1 = (math.log(self.stock.spot / self.strike) + (self.stock.rate - \
             self.stock.divid + 0.5 * (self.stock.vol**2)) * self.expiry) / \
@@ -19,33 +20,39 @@ class VanillaCall:
         self.strike = strike
         self.expiry = expiry
 
-    price = _normal_limits(lambda self, d_1, d_2: \
-        self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
+    @_normal_cdf_limits
+    def price(self, d_1, d_2): \
+        return self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
         norm.cdf(d_1) - self.strike * math.exp(-self.stock.rate * \
-        self.expiry) * norm.cdf(d_2))
+        self.expiry) * norm.cdf(d_2)
 
-    delta = _normal_limits(lambda self, d_1, d_2: \
-        math.exp(-self.stock.divid * self.expiry) * norm.cdf(d_1))
+    @_normal_cdf_limits
+    def delta(self, d_1, d_2): \
+        return math.exp(-self.stock.divid * self.expiry) * norm.cdf(d_1)
 
-    gamma = _normal_limits(lambda self, d_1, d_2: \
-        (math.exp(-self.stock.divid * self.expiry) * _gaussian(d_1)) / \
-        (self.stock.spot * self.stock.vol * math.sqrt(self.expiry)))
+    @_normal_cdf_limits
+    def gamma(self, d_1, d_2): \
+        return (math.exp(-self.stock.divid * self.expiry) * _gaussian(d_1)) / \
+        (self.stock.spot * self.stock.vol * math.sqrt(self.expiry))
 
-    vega = _normal_limits(lambda self, d_1, d_2: \
-        self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
-        math.sqrt(self.expiry) * _gaussian(d_1))
+    @_normal_cdf_limits
+    def vega(self, d_1, d_2): \
+        return self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
+        math.sqrt(self.expiry) * _gaussian(d_1)
 
-    rho = _normal_limits(lambda self, d_1, d_2: \
-        self.strike * math.exp(-self.stock.rate * self.expiry) * \
-        self.expiry * norm.cdf(d_2))
+    @_normal_cdf_limits
+    def rho(self, d_1, d_2): \
+        return self.strike * math.exp(-self.stock.rate * self.expiry) * \
+        self.expiry * norm.cdf(d_2)
 
-    theta = _normal_limits(lambda self, d_1, d_2: \
-        self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
+    @_normal_cdf_limits
+    def theta(self, d_1, d_2): \
+        return self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
         self.stock.divid * norm.cdf(d_1) - self.strike * \
         math.exp(-self.stock.rate * self.expiry) * self.stock.rate * \
         norm.cdf(d_2) - self.stock.spot * math.exp(-self.stock.divid * \
         self.expiry) * (self.stock.vol / (2 * math.sqrt(self.expiry))) * \
-        _gaussian(d_1))
+        _gaussian(d_1)
 
 class VanillaPut:
     def __init__(self, stock, expiry, strike):
@@ -53,30 +60,36 @@ class VanillaPut:
         self.strike = strike
         self.expiry = expiry
 
-    price = _normal_limits(lambda self, d_1, d_2: \
-        -self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
+    @_normal_cdf_limits
+    def price(self, d_1, d_2): \
+        return -self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
         norm.cdf(-d_1) + self.strike * math.exp(-self.stock.rate * \
-        self.expiry) * norm.cdf(-d_2))
+        self.expiry) * norm.cdf(-d_2)
 
-    delta = _normal_limits(lambda self, d_1, d_2: \
-        -math.exp(-self.stock.divid * self.expiry) * norm.cdf(-d_1))
+    @_normal_cdf_limits
+    def delta(self, d_1, d_2): \
+        return -math.exp(-self.stock.divid * self.expiry) * norm.cdf(-d_1)
 
-    gamma = _normal_limits(lambda self, d_1, d_2: \
-        (math.exp(-self.stock.divid * self.expiry) * _gaussian(d_1)) / \
-        (self.stock.spot * self.stock.vol * math.sqrt(self.expiry)))
+    @_normal_cdf_limits
+    def gamma(self, d_1, d_2): \
+        return (math.exp(-self.stock.divid * self.expiry) * _gaussian(d_1)) / \
+        (self.stock.spot * self.stock.vol * math.sqrt(self.expiry))
 
-    vega = _normal_limits(lambda self, d_1, d_2: \
-        self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
-        math.sqrt(self.expiry) * _gaussian(d_1))
+    @_normal_cdf_limits
+    def vega(self, d_1, d_2): \
+        return self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
+        math.sqrt(self.expiry) * _gaussian(d_1)
 
-    rho = _normal_limits(lambda self, d_1, d_2: \
-        -self.strike * math.exp(-self.stock.rate * self.expiry) * \
-        self.expiry * norm.cdf(-d_2))
+    @_normal_cdf_limits
+    def rho(self, d_1, d_2): \
+        return -self.strike * math.exp(-self.stock.rate * self.expiry) * \
+        self.expiry * norm.cdf(-d_2)
 
-    theta = _normal_limits(lambda self, d_1, d_2: \
-        -self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
+    @_normal_cdf_limits
+    def theta(self, d_1, d_2): \
+        return -self.stock.spot * math.exp(-self.stock.divid * self.expiry) * \
         self.stock.divid * norm.cdf(-d_1) + self.strike * \
         math.exp(-self.stock.rate * self.expiry) * self.stock.rate * \
         norm.cdf(-d_2) - self.stock.spot * math.exp(-self.stock.divid * \
         self.expiry) * (self.stock.vol / (2 * math.sqrt(self.expiry))) * \
-        _gaussian(d_1))
+        _gaussian(d_1)
