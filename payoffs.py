c013f4f78194ea57ac96467b22b7960b5076c39b
Rishabh Ballal <rishabhballal@gmail.com>
2025-12-03 17:50:22 +0530

    feat: add lookback option payoffs; style: replace lambda functions

--- payoffs.py
+++ payoffs.py
@@ -1,26 +1,47 @@
 import numpy as np
 
 # European, Bermudan, American option payoffs
-forward_contract = lambda K: (lambda S: S-K)
+def forward_contract(K):
+    return (lambda S: S-K)
 
-vanilla_call = lambda K: (lambda S: S-K if S>K else 0)
-vanilla_put = lambda K: (lambda S: K-S if S<K else 0)
+def vanilla_call(K):
+    return (lambda S: S-K if S>K else 0)
+def vanilla_put(K):
+    return (lambda S: K-S if S<K else 0)
 
-digital_call = lambda K: (lambda S: 1 if S>K else 0)
-digital_put = lambda K: (lambda S: 1 if S<K else 0)
+def digital_call(K):
+    return (lambda S: 1 if S>K else 0)
+def digital_put(K):
+    return (lambda S: 1 if S<K else 0)
 
-power_call = lambda K, l: (lambda S: (S-K)^l if S>K else 0)
-power_put = lambda K, l: (lambda S: (K-S)^l if S<K else 0)
+def power_call(K, l):
+    return (lambda S: (S-K)^l if S>K else 0)
+def power_put(K, l):
+    return (lambda S: (K-S)^l if S<K else 0)
 
-straddle = lambda K: (lambda S: S-K if S>K else K-S)
+def straddle(K):
+    return (lambda S: S-K if S>K else K-S)
 
 # Asian option payoffs
-arithmetic_asian_call = lambda K: (lambda S: sum(S)/len(S) - K \
-    if sum(S)/len(S) > K else 0)
-arithmetic_asian_put = lambda K: (lambda S: K - sum(S)/len(S) \
-    if sum(S)/len(S) < K else 0)
+def arithmetic_asian_call(K):
+    return (lambda S: sum(S)/len(S) - K if sum(S)/len(S) > K else 0)
+def arithmetic_asian_put(K):
+    return (lambda S: K - sum(S)/len(S) if sum(S)/len(S) < K else 0)
 
-geometric_asian_call = lambda K: (lambda S: np.prod(S)**(1/len(S)) - K \
-    if np.prod(S)**(1/len(S)) > K else 0)
-geometric_asian_put = lambda K: (lambda S: K - np.prod(S)**(1/len(S)) \
-    if np.prod(S)**(1/len(S)) < K else 0)
+def geometric_asian_call(K):
+    return (lambda S: np.prod(S)**(1/len(S)) - K if np.prod(S)**(1/len(S)) > K \
+        else 0)
+def geometric_asian_put(K):
+    return (lambda S: K - np.prod(S)**(1/len(S)) if np.prod(S)**(1/len(S)) < K \
+        else 0)
+
+# lookback option payoffs
+def lookback_call_fixed(K):
+    return (lambda S, S_min, S_max: S_max - K if S_max > K else 0)
+def lookback_put_fixed(K):
+    return (lambda S, S_min, S_max: K - S_min if S_min < K else 0)
+
+def lookback_call_floating():
+    return (lambda S, S_min, S_max: S - S_min)
+def lookback_put_floating():
+    return (lambda S, S_min, S_max: S_max - S)
