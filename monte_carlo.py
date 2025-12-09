b923a5319bf22f3a3ecb465279c9cece050b4d39
Rishabh Ballal <rishabhballal@gmail.com>
2025-12-09 19:10:54 +0530

    feat: restructure option classes based on path dependence

--- monte_carlo.py
+++ monte_carlo.py
@@ -2,195 +2,147 @@ import math
 import numpy as np
 
 rng = np.random.default_rng()
+count = 10**5
+_dt = 1/252
 
 class Stock:
-    def __init__(self, spot=100, rate=0.05, divid=0.03, vol=0.10, count=10**4):
-        self.count = count
+    def __init__(self, spot=100, rate=0.05, divid=0.03, vol=0.10):
         self.spot = spot
         self.rate = rate
         self.divid = divid
         self.vol = vol
 
-    def geom_brownian(self, time=1, steps=10**2, rand=[]):
+    def gbm_paths(self, time=252, rand=[]):
         if not len(rand):
-            rand = rng.standard_normal((steps, self.count))
-        S = np.ones((steps+1, self.count)) * self.spot
-        if steps == 1:
-            S[-1] = [self.spot * math.exp((self.rate - self.divid) * time - \
-                0.5 * (self.vol**2) * time + self.vol * math.sqrt(time) * x) \
-                for x in rand[-1]]
-        else:
-            for i in range(steps):
-                S[i+1] = S[i] * (1 + (self.rate - self.divid) * time / \
-                    steps + self.vol * math.sqrt(time / steps) * rand[i])
-        return S
+            rand = rng.standard_normal((time, count))
+        paths = np.ones((1+time, count)) * self.spot
+        for i in range(time):
+            paths[i+1] = paths[i] * np.exp((self.rate - self.divid) * _dt - \
+                self.vol**2 * _dt / 2 + self.vol * math.sqrt(_dt) * rand[i])
+        return paths
 
-class EuropeanOption:
+class _OptionGreeks:
+    def __init__(self, option):
+        self.option = option
+
+    def delta(self, rand):
+        epsilon = 0.01
+        self.option.stock.spot += epsilon
+        price_eps = self.option.price(rand)
+        self.option.stock.spot -= epsilon
+        return (price_eps - self.option.price(rand)) / epsilon
+
+    def gamma(self, rand):
+        epsilon = 0.01
+        self.option.stock.spot += epsilon
+        delta_eps = self.option.delta(rand)
+        self.option.stock.spot -= epsilon
+        return (delta_eps - self.option.delta(rand)) / epsilon
+
+    def vega(self, rand):
+        epsilon = 0.0001
+        self.option.stock.vol += epsilon
+        price_eps = self.option.price(rand)
+        self.option.stock.vol -= epsilon
+        return (price_eps - self.option.price(rand)) / epsilon
+
+    def rho(self, rand):
+        epsilon = 0.0001
+        self.option.stock.rate += epsilon
+        price_eps = self.option.price(rand)
+        self.option.stock.rate -= epsilon
+        return (price_eps - self.option.price(rand)) / epsilon
+
+    def theta(self, rand):
+        try:
+            self.option.expiry -= 1
+            price_eps = self.option.price(rand)
+            self.option.expiry += 1
+        except AttributeError:
+            self.option.times = [x-1 for x in self.option.times]
+            price_eps = self.option.price(rand)
+            self.option.times = [x+1 for x in self.option.times]
+        return (price_eps - self.option.price(rand)) / _dt
+
+class PathIndependentEuropeanOption:
     def __init__(self, stock, expiry, payoff):
         self.stock = stock
         self.expiry = expiry
         self.payoff = lambda S: [payoff(x) for x in S]
+        self.greeks = _OptionGreeks(self)
 
     def _random_seed(func):
-        def wrapper(self, steps=10**3, rand=[]):
+        def wrapper(self, rand=[]):
             if not len(rand):
-                rand = rng.standard_normal((steps, self.stock.count))
-            return func(self, steps, rand)
+                rand = rng.standard_normal((self.expiry, count))
+            return func(self, rand)
         return wrapper
 
     @_random_seed
-    def price(self, steps, rand):
-        S_T = self.stock.geom_brownian(self.expiry, steps, rand)[-1]
-        return math.exp(-self.stock.rate * self.expiry) * \
-            np.mean(self.payoff(S_T))
+    def price(self, rand):
+        paths = self.stock.gbm_paths(self.expiry, rand)
+        return math.exp(-self.stock.rate * self.expiry * _dt) * \
+            np.mean(self.payoff(paths[-1]))
 
     @_random_seed
-    def delta(self, steps, rand):
-        epsilon = 0.01
-        self.stock.spot += epsilon
-        price_eps = self.price(steps, rand)
-        self.stock.spot -= epsilon
-        return (price_eps - self.price(steps, rand)) / epsilon
+    def delta(self, rand):
+        return self.greeks.delta(rand)
 
     @_random_seed
-    def gamma(self, steps, rand):
-        epsilon = 0.01
-        self.stock.spot += epsilon
-        delta_eps = self.delta(steps, rand)
-        self.stock.spot -= epsilon
-        return (delta_eps - self.delta(steps, rand)) / epsilon
+    def gamma(self, rand):
+        return self.greeks.gamma(rand)
 
     @_random_seed
-    def vega(self, steps, rand):
-        epsilon = 0.0001
-        self.stock.vol += epsilon
-        price_eps = self.price(steps, rand)
-        self.stock.vol -= epsilon
-        return (price_eps - self.price(steps, rand)) / epsilon
+    def vega(self, rand):
+        return self.greeks.vega(rand)
 
     @_random_seed
-    def rho(self, steps, rand):
-        epsilon = 0.0001
-        self.stock.rate += epsilon
-        price_eps = self.price(steps, rand)
-        self.stock.rate -= epsilon
-        return (price_eps - self.price(steps, rand)) / epsilon
+    def rho(self, rand):
+        return self.greeks.rho(rand)
 
     @_random_seed
-    def theta(self, steps, rand):
-        epsilon = 0.01
-        self.expiry -= epsilon
-        price_eps = self.price(steps, rand)
-        self.expiry += epsilon
-        return (price_eps - self.price(steps, rand)) / epsilon
+    def theta(self, rand):
+        return self.greeks.theta(rand)
 
-class LookbackOption:
-    def __init__(self, stock, expiry, payoff):
-        self.stock = stock
-        self.expiry = expiry
-        self.payoff = lambda S, S_min, S_max: [payoff(S[i], S_min[i], S_max[i]) for i in range(self.stock.count)]
-
-    def _random_seed(func):
-        def wrapper(self, steps=10**3, rand=[]):
-            if not len(rand):
-                rand = rng.standard_normal((steps, self.stock.count))
-            return func(self, steps, rand)
-        return wrapper
-
-    @_random_seed
-    def price(self, steps, rand):
-        S = self.stock.geom_brownian(self.expiry, steps, rand)
-        S_min = [min(S[:,i]) for i in range(self.stock.count)]
-        S_max = [max(S[:,i]) for i in range(self.stock.count)]
-        return math.exp(-self.stock.rate * self.expiry) * \
-            np.mean(self.payoff(S[-1], S_min, S_max))
-
-class AsianOption:
+class PathDependentEuropeanOption:
     def __init__(self, stock, times, payoff):
         self.stock = stock
         self.times = times
-        self.payoff = lambda S: [payoff(S[:,i]) for i in range(stock.count)]
+        self.payoff = lambda S: [payoff(S[:,i]) for i in range(len(S[-1]))]
+        self.greeks = _OptionGreeks(self)
 
     def _random_seed(func):
-        def wrapper(self, steps_=10**2, rand=[]):
+        def wrapper(self, rand=[]):
             if not len(rand):
-                rand = rng.standard_normal((steps_ * len(self.times), \
-                    self.stock.count))
-            return func(self, steps_, rand)
+                rand = rng.standard_normal((self.times[-1], count))
+            return func(self, rand)
         return wrapper
 
     @_random_seed
-    def price(self, steps_, rand):
-        total_steps = steps_ * len(self.times)
-        S = self.stock.geom_brownian(self.times[-1], total_steps, rand)
-        for i in reversed(range(total_steps+1)):
-            if i * self.times[-1] / total_steps not in self.times:
-                S = np.delete(S, i, axis=0)
-        return math.exp(-self.stock.rate * self.times[-1]) * \
-            np.mean(self.payoff(S))
+    def price(self, rand):
+        paths = self.stock.gbm_paths(self.times[-1], rand)
+        for i in reversed(range(self.times[-1] + 1)):
+            if i not in self.times:
+                paths = np.delete(paths, i, axis=0)
+        return math.exp(-self.stock.rate * self.times[-1] * _dt) * \
+            np.mean(self.payoff(paths))
 
     @_random_seed
-    def delta(self, steps_, rand):
-        epsilon = 0.01
-        self.stock.spot += epsilon
-        price_eps = self.price(steps_, rand)
-        self.stock.spot -= epsilon
-        return (price_eps - self.price(steps_, rand)) / epsilon
+    def delta(self, rand):
+        return self.greeks.delta(rand)
 
     @_random_seed
-    def gamma(self, steps_, rand):
-        epsilon = 0.01
-        self.stock.spot += epsilon
-        delta_eps = self.delta(steps_, rand)
-        self.stock.spot -= epsilon
-        return (delta_eps - self.delta(steps_, rand)) / epsilon
+    def gamma(self, rand):
+        return self.greeks.gamma(rand)
 
     @_random_seed
-    def vega(self, steps_, rand):
-        epsilon = 0.0001
-        self.stock.vol += epsilon
-        price_eps = self.price(steps_, rand)
-        self.stock.vol -= epsilon
-        return (price_eps - self.price(steps_, rand)) / epsilon
+    def vega(self, rand):
+        return self.greeks.vega(rand)
 
     @_random_seed
-    def rho(self, steps_, rand):
-        epsilon = 0.0001
-        self.stock.rate += epsilon
-        price_eps = self.price(steps_, rand)
-        self.stock.rate -= epsilon
-        return (price_eps - self.price(steps_, rand)) / epsilon
+    def rho(self, rand):
+        return self.greeks.rho(rand)
 
     @_random_seed
-    def theta(self, steps_, rand):
-        epsilon = 0.01
-        for i in range(len(self.times)):
-            self.times[i] -= epsilon * (i+1)
-        price_eps = self.price(steps_, rand)
-        for i in range(len(self.times)):
-            self.times[i] += epsilon * (i+1)
-        return (price_eps - self.price(steps_, rand)) / epsilon
-
-class DiscreteBarrierOption:
-    def __init__(self, stock, times, payoff):
-        self.stock = stock
-        self.times = times
-        self.payoff = lambda S: [payoff(S[:,i]) for i in range(stock.count)]
-
-    def _random_seed(func):
-        def wrapper(self, steps_=10**2, rand=[]):
-            if not len(rand):
-                rand = rng.standard_normal((steps_ * len(self.times), \
-                    self.stock.count))
-            return func(self, steps_, rand)
-        return wrapper
-
-    @_random_seed
-    def price(self, steps_, rand):
-        total_steps = steps_ * len(self.times)
-        S = self.stock.geom_brownian(self.times[-1], total_steps, rand)
-        for i in reversed(range(total_steps+1)):
-            if i * self.times[-1] / total_steps not in self.times:
-                S = np.delete(S, i, axis=0)
-        return math.exp(-self.stock.rate * self.times[-1]) * \
-            np.mean(self.payoff(S))
+    def theta(self, rand):
+        return self.greeks.theta(rand)
