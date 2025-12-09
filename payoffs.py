8703aeadebcec5122ea537b2f81c68e5ddb1654c
Rishabh Ballal <rishabhballal@gmail.com>
2025-12-09 19:08:56 +0530

    style: rename args to clarify spot and path dependence

--- payoffs.py
+++ payoffs.py
@@ -1,66 +1,76 @@
 import numpy as np
 
-# usual payoffs for instantiating EuropeanOtion, AmericanOption, BermudanOption
+# some payoffs for instantiating trees.EuropeanOption, trees.AmericanOption,
+# trees.BermudanOption, monte_carlo.PathIndependentEuropeanOption
+
 def forward_contract(strike):
-    return (lambda S: S - strike)
+    return (lambda spot: spot - strike)
 
 def vanilla_call(strike):
-    return (lambda S: max(S - strike, 0))
+    return (lambda spot: max(spot - strike, 0))
 def vanilla_put(strike):
-    return (lambda S: max(strike - S, 0))
+    return (lambda spot: max(strike - spot, 0))
 
 def digital_call(strike):
-    return (lambda S: 1 if S > strike else 0)
+    return (lambda spot: 1 if spot > strike else 0)
 def digital_put(strike):
-    return (lambda S: 1 if S < strike else 0)
+    return (lambda spot: 1 if spot < strike else 0)
 
 def power_call(strike, power):
-    return (lambda S: (S - strike)**power if S > strike else 0)
+    return (lambda spot: (spot - strike)**power if spot > strike else 0)
 def power_put(strike, power):
-    return (lambda S: (strike - S)**power if S < strike else 0)
+    return (lambda spot: (strike - spot)**power if spot < strike else 0)
 
 def straddle(strike):
-    return (lambda S: max(S - strike, strike - S))
+    return (lambda spot: max(spot - strike, strike - spot))
 
-# usual payoffs for instantiating AsianOption
+
+# some payoffs for instantiating monte_carlo.PathDependentEuropeanOption
+
+def fixed_lookback_call(strike):
+    return (lambda path: max(np.max(path) - strike, 0))
+def fixed_lookback_put(strike):
+    return (lambda path: max(strike - np.min(path), 0))
+
+def floating_lookback_call():
+    return (lambda path: path[-1] - np.min(path))
+def floating_lookback_put():
+    return (lambda path: np.max(path) - path[-1])
+
 def arithmetic_asian_call(strike):
-    return (lambda S: max(sum(S)/len(S) - strike, 0))
+    return (lambda path: max(np.sum(path) / np.size(path) - strike, 0))
 def arithmetic_asian_put(strike):
-    return (lambda S: max(strike - sum(S)/len(S), 0))
+    return (lambda path: max(strike - np.sum(path) / np.size(path), 0))
 
 def geometric_asian_call(strike):
-    return (lambda S: max(np.prod(S)**(1/len(S)) - strike, 0))
+    return (lambda path: max(np.prod(path)**(1 / np.size(path)) - strike, 0))
 def geometric_asian_put(strike):
-    return (lambda S: max(strike - np.prod(S)**(1/len(S)), 0))
+    return (lambda path: max(strike - np.prod(path)**(1 / np.size(path)), 0))
 
-# usual payoffs for instantiating LookbackOption
-def lookback_call_fixed(strike):
-    return (lambda S, S_min, S_max: max(S_max - strike, 0))
-def lookback_put_fixed(strike):
-    return (lambda S, S_min, S_max: max(strike - S_min, 0))
-
-def lookback_call_floating():
-    return (lambda S, S_min, S_max: S - S_min)
-def lookback_put_floating():
-    return (lambda S, S_min, S_max: S_max - S)
-
-# usual payoffs for instantiating DiscreteBarrierOption
 def discrete_down_and_out_call(strike, barrier):
-    return (lambda S: max(S[-1] - strike, 0) if np.all(S > barrier) else 0)
+    return (lambda path: max(path[-1] - strike, 0) \
+        if np.all(path > barrier) else 0)
 def discrete_down_and_out_put(strike, barrier):
-    return (lambda S: max(strike - S[-1], 0) if np.all(S > barrier) else 0)
+    return (lambda path: max(strike - path[-1], 0) \
+        if np.all(path > barrier) else 0)
 
 def discrete_down_and_in_call(strike, barrier):
-    return (lambda S: max(S[-1] - strike, 0) if np.any(S < barrier) else 0)
+    return (lambda path: max(path[-1] - strike, 0) \
+        if np.any(path < barrier) else 0)
 def discrete_down_and_in_put(strike, barrier):
-    return (lambda S: max(strike - S[-1], 0) if np.any(S < barrier) else 0)
+    return (lambda path: max(strike - path[-1], 0) \
+        if np.any(path < barrier) else 0)
 
 def discrete_up_and_out_call(strike, barrier):
-    return (lambda S: max(S[-1] - strike, 0) if np.all(S < barrier) else 0)
+    return (lambda path: max(path[-1] - strike, 0) \
+        if np.all(path < barrier) else 0)
 def discrete_up_and_out_put(strike, barrier):
-    return (lambda S: max(strike - S[-1], 0) if np.all(S < barrier) else 0)
+    return (lambda path: max(strike - path[-1], 0) \
+        if np.all(path < barrier) else 0)
 
 def discrete_up_and_in_call(strike, barrier):
-    return (lambda S: max(S[-1] - strike, 0) if np.any(S > barrier) else 0)
+    return (lambda path: max(path[-1] - strike, 0) \
+        if np.any(path > barrier) else 0)
 def discrete_up_and_in_put(strike, barrier):
-    return (lambda S: max(strike - S[-1], 0) if np.any(S > barrier) else 0)
+    return (lambda path: max(strike - path[-1], 0) \
+        if np.any(path > barrier) else 0)
